312bb9fe0af0bfc1dae0098b1632bf7267ffb88c
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2024-12-15 18:19:59 +0100

    testig const

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+/// The major version of the FEF specification this library version is based on.
+///
+/// Example:
+/// ```
+/// use fef::SPECIFICATION_MAJOR_VERSION;
+///
+/// assert_eq!(SPECIFICATION_MAJOR_VERSION, 0);
+/// ```
+pub const SPECIFICATION_MAJOR_VERSION: u32 = 0;
