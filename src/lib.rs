df4d75e3de3cec32fcb8f4d955c162fc703d569e
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2024-12-15 18:20:56 +0100

    01 cargo package init (#1)
    * package init
    
    * license
    
    * keywords and categories
    
    * adding cargo lock to gitignore
    
    * testig const

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+/// The major version of the FEF specification this library version is based on.
+///
+/// Example:
+/// ```
+/// use fef::SPECIFICATION_MAJOR_VERSION;
+///
+/// assert_eq!(SPECIFICATION_MAJOR_VERSION, 0);
+/// ```
+pub const SPECIFICATION_MAJOR_VERSION: u32 = 0;
