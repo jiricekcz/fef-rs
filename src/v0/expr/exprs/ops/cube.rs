ffe251adcbdf33f3496c223c7ffbb40f015227cb
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2025-01-26 20:06:28 +0100

    cube

---
+++ src/v0/expr/exprs/ops/cube.rs
@@ -0,0 +1,62 @@
+use crate::{
+    common::traits::private::Sealed,
+    v0::{
+        expr::{
+            error::NonMatchingExprError,
+            traits::{ExprObj, UnaryOperationExprObj},
+            Expr,
+        },
+        tokens::ExprToken,
+    },
+};
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct ExprCube<S: Sized> {
+    operand: S,
+}
+
+impl<S: Sized> From<S> for ExprCube<S> {
+    fn from(operand: S) -> Self {
+        Self { operand }
+    }
+}
+
+impl<S: Sized> Sealed for ExprCube<S> {}
+
+impl<S: Sized> Into<Expr<S>> for ExprCube<S> {
+    fn into(self) -> Expr<S> {
+        Expr::Cube(self)
+    }
+}
+
+impl<S: Sized> TryFrom<Expr<S>> for ExprCube<S> {
+    type Error = NonMatchingExprError;
+
+    fn try_from(expr: Expr<S>) -> Result<Self, Self::Error> {
+        match expr {
+            Expr::Cube(expr) => Ok(expr),
+            _ => Err(NonMatchingExprError {
+                expected: ExprToken::Cube,
+                found: expr.token(),
+            }),
+        }
+    }
+}
+
+impl<S: Sized> ExprObj<S> for ExprCube<S> {
+    fn token(&self) -> ExprToken {
+        ExprToken::Cube
+    }
+}
+
+impl<S: Sized> UnaryOperationExprObj<S> for ExprCube<S> {
+    fn inner(&self) -> &S {
+        &self.operand
+    }
+    fn inner_mut(&mut self) -> &mut S {
+        &mut self.operand
+    }
+    fn into_inner(self) -> S {
+        self.operand
+    }
+}
--- src/v0/expr/exprs/ops/mod.rs
+++ src/v0/expr/exprs/ops/mod.rs
@@ -1,4 +1,5 @@
 mod addition;
+mod cube;
 mod division;
 mod int_division;
 mod int_root;
@@ -11,6 +12,7 @@ mod square;
 mod subtraction;
 
 pub use addition::ExprAddition;
+pub use cube::ExprCube;
 pub use division::ExprDivision;
 pub use int_division::ExprIntDivision;
 pub use int_root::ExprIntRoot;
