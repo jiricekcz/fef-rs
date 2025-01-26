e35f5b80db48677f9882340734e972d5bc1ee9e8
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2025-01-26 19:49:25 +0100

    Imports

--- src/v0/expr/exprs/ops/mod.rs
+++ src/v0/expr/exprs/ops/mod.rs
@@ -1,7 +1,11 @@
 mod addition;
 mod division;
+mod int_division;
+mod multiplication;
 mod subtraction;
 
 pub use addition::ExprAddition;
 pub use division::ExprDivision;
+pub use int_division::ExprIntDivision;
+pub use multiplication::ExprMultiplication;
 pub use subtraction::ExprSubtraction;
