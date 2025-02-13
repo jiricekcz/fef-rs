e9e7a851ac0ef63878d88156a48691685bf8c3e8
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2025-02-13 21:27:48 +0100

    default config docs

--- src/v0/config/default.rs
+++ src/v0/config/default.rs
@@ -3,6 +3,16 @@ use super::Config;
 /// Configuration object with default values.
 ///
 /// It is useful for example to parse the configuration of a given file.
+///
+/// # Example
+/// All of the configuration values are set to their default values.
+/// ```rust
+/// # use fef::v0::config::{IntFormat, FloatFormat, DefaultConfig, Config};
+/// let config = DefaultConfig::new();
+///
+/// assert_eq!(config.float_format(), FloatFormat::default());
+/// assert_eq!(config.integer_format(), IntFormat::default());
+/// ```
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct DefaultConfig {}
 impl Config for DefaultConfig {}
@@ -15,10 +25,13 @@ impl Default for DefaultConfig {
 }
 
 impl DefaultConfig {
-    /// Creates a new instance of the default configuration. Equivalent to using the [Default] trait.
+    /// Creates a new instance of the default configuration. Equivalent to using the [`Default`] trait.
     pub const fn new() -> Self {
         DefaultConfig {}
     }
 }
 
+/// Default configuration object.
+///
+/// This is an instance of the [`DefaultConfig`] struct, which can be used instead of creating a new instance.
 pub const DEFAULT_CONFIG: DefaultConfig = DefaultConfig::new();
