cf2720fa8dc022bb16f20cd055f1cd43bbdd04a1
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2025-02-13 17:08:56 +0100

    added parse_metadata_as_vec for ease of use

--- src/v0/parse/mod.rs
+++ src/v0/parse/mod.rs
@@ -11,6 +11,7 @@ pub use configuration::parse_configuration;
 pub use configuration::parse_configuration_with_default_configuration;
 
 pub use metadata::parse_metadata;
+pub use metadata::parse_metadata_as_vec;
 
 pub use file::read_file;
 pub use file::read_file_with_default_config;
