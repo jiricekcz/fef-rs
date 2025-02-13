2dee8aff39ece3b92bd8510b76920b0f31dc5fe3
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2025-02-13 16:17:35 +0100

    DefaultConfig public

--- src/v0/config/mod.rs
+++ src/v0/config/mod.rs
@@ -6,6 +6,7 @@ mod read_configuration;
 mod traits;
 
 pub use configurations::*;
+pub use default::DefaultConfig;
 pub use default::DEFAULT_CONFIG;
 pub use overridable_config::OverridableConfig;
 pub use read_configuration::ConfigurationValue;
