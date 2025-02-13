98f8347496a50b2545c88d3812479b0914476b8e
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2025-02-13 16:08:12 +0100

    removed value_for_token from public API

--- src/v0/write/configuration.rs
+++ src/v0/write/configuration.rs
@@ -7,9 +7,9 @@ use crate::v0::{
 };
 
 macro_rules! write_enum_configuration {
-    ($configuration_type:ty, $writer:ident, $configuration:ident) => {{
+    ($configuration_type:ty, $config_method:ident, $writer:ident, $configuration:ident) => {{
         let key = <$configuration_type>::token();
-        let value = $configuration.value_for_token(key);
+        let value = $configuration.$config_method().value();
         key.variable_length_enum()
             .write_to($writer, $configuration)?;
         VariableLengthEnum::from(value).write_to($writer, $configuration)?;
@@ -20,7 +20,7 @@ pub fn write_configuration<W: ?Sized + Write, C: ?Sized + Config>(
     writer: &mut W,
     configuration: &C,
 ) -> Result<(), ConfigurationWriteError> {
-    write_enum_configuration!(IntFormat, writer, configuration);
-    write_enum_configuration!(FloatFormat, writer, configuration);
+    write_enum_configuration!(IntFormat, integer_format, writer, configuration);
+    write_enum_configuration!(FloatFormat, float_format, writer, configuration);
     Ok(())
 }
