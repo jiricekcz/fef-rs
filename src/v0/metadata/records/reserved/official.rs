cc1c7618869fd3240dd3eab0995202958c75c7fd
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2025-02-13 22:38:00 +0100

    docs for official reserved metadata record

--- src/v0/metadata/records/reserved/official.rs
+++ src/v0/metadata/records/reserved/official.rs
@@ -14,6 +14,12 @@ use crate::{
     },
 };
 
+/// Metadata record with identifier reserved for official use by the FEF specification.
+///
+/// These keys are reserved for future use by the FEF specification.
+/// They may be defined later. If this record is encountered, it should be ignored - either the data is misformed or the library is outdated.
+/// Make sure that encountering this record does not cause a failure in your application - applications should guarantee forward compatibility when
+/// encountering unknown metadata keys.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct OfficialReservedMetadataRecordObj {
     identifier: u32,
@@ -46,6 +52,11 @@ impl OfficialReservedMetadataRecordObj {
 
 impl<W: ?Sized + Write> WriteTo<W> for OfficialReservedMetadataRecordObj {
     type WriteError = MetadataRecordWriteError;
+
+    /// Writes the metadata record to a writer.
+    ///
+    /// This method should rarely be used, as it creates a situation where the library is on purpose creating a record that is not defined in the specification.
+    /// This is for example useful, if you want to copy a record or convert it from a different format - this way you don't need to know the exact structure of the record.
     fn write_to<C: ?Sized + Config>(
         &self,
         writer: &mut W,
