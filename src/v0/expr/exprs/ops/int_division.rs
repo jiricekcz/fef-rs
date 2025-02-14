de743bbb0fe4192663bf51d07140366e13633f57
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2025-02-14 16:22:31 +0100

    docs for int division

--- src/v0/expr/exprs/ops/int_division.rs
+++ src/v0/expr/exprs/ops/int_division.rs
@@ -10,18 +10,21 @@ use crate::{
     },
 };
 
+/// [Integer division expression](https://github.com/jiricekcz/fef-specification/blob/main/expressions/Integer%20Division.md) in FEF.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct ExprIntDivision<S: Sized> {
     lhs: S,
     rhs: S,
 }
 
+/// Creates an integer division expression from its left-hand side and right-hand side.
 impl<S: Sized> From<(S, S)> for ExprIntDivision<S> {
     fn from((lhs, rhs): (S, S)) -> Self {
         Self { lhs, rhs }
     }
 }
 
+/// Converts the integer division expression into its left-hand side and right-hand side.
 impl<S: Sized> Into<(S, S)> for ExprIntDivision<S> {
     fn into(self) -> (S, S) {
         (self.lhs, self.rhs)
@@ -63,5 +66,13 @@ impl<S: Sized> BinaryOperationExpr<S> for ExprIntDivision<S> {
 
     fn rhs(&self) -> &S {
         &self.rhs
+    }
+
+    fn lhs_mut(&mut self) -> &mut S {
+        &mut self.lhs
+    }
+
+    fn rhs_mut(&mut self) -> &mut S {
+        &mut self.rhs
     }
 }
