2c1b5b32b45a66a995bac1de510af854d3d81e3f
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2025-02-14 16:52:50 +0100

    docs for parse configuration

--- src/v0/parse/configuration.rs
+++ src/v0/parse/configuration.rs
@@ -5,6 +5,39 @@ use crate::v0::{
     traits::ReadFrom,
 };
 
+/// Reads a configuration from a byte stream using some configuration.
+///
+/// This is the most generic method for reading configuration. It assumes
+/// that your application has some sort of default configuration that you
+/// want to use instead of the default configuration provided by the standard.
+///
+/// Other than that, it functions the same as [`parse_configuration_with_default_configuration`].
+///
+/// # Example
+/// ```rust
+/// # use fef::v0::parse::parse_configuration;
+/// # use fef::v0::config::Config;
+/// # use fef::v0::config::IntFormat;
+/// # use fef::v0::config::FloatFormat;
+/// # use fef::v0::config::OverridableConfig;
+/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// let bytes: Vec<u8> = vec![
+///     0x02, // 2 configurations
+///     0x01, 0x01, // Integer format: `i16`,
+///     0x00, 0x01, // Float format: `f32`,
+/// ];
+///
+/// let config = OverridableConfig::default();
+/// // Edit the configuration to your liking
+///
+/// let mut reader = &mut bytes.as_slice();
+/// let config = parse_configuration(&mut reader, &config)?;
+///
+/// assert_eq!(config.integer_format(), IntFormat::I16);
+/// assert_eq!(config.float_format(), FloatFormat::F32);
+/// # assert!(reader.is_empty());
+/// # Ok(())
+/// # }
 pub fn parse_configuration<R: ?Sized + Read, C: ?Sized + Config>(
     byte_stream: &mut R,
     configuration: &C,
@@ -12,6 +45,32 @@ pub fn parse_configuration<R: ?Sized + Read, C: ?Sized + Config>(
     OverridableConfig::read_from(byte_stream, configuration)
 }
 
+/// Reads a configuration from a byte stream using the default configuration.
+///
+/// This is the most common way of reading configurations of files you know
+/// nothing about. It uses the default configuration provided by the standard.
+///
+/// # Example
+/// ```rust
+/// # use fef::v0::parse::parse_configuration_with_default_configuration;
+/// # use fef::v0::config::Config;
+/// # use fef::v0::config::IntFormat;
+/// # use fef::v0::config::FloatFormat;
+/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// let bytes: Vec<u8> = vec![
+///     0x02, // 2 configurations
+///     0x01, 0x01, // Integer format: `i16`,
+///     0x00, 0x01, // Float format: `f32`,
+/// ];
+///
+/// let mut reader = &mut bytes.as_slice();
+/// let config = parse_configuration_with_default_configuration(&mut reader)?;
+///
+/// assert_eq!(config.integer_format(), IntFormat::I16);
+/// assert_eq!(config.float_format(), FloatFormat::F32);
+/// # assert!(reader.is_empty());
+/// # Ok(())
+/// # }
 pub fn parse_configuration_with_default_configuration<R: ?Sized + Read>(
     byte_stream: &mut R,
 ) -> Result<OverridableConfig, ConfigurationReadError> {
