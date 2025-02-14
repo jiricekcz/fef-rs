75d7787149d5dfa3c9e908722d7c02657a24bb69
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2025-02-14 14:00:00 +0100

    expr root docs

--- src/v0/expr/mod.rs
+++ src/v0/expr/mod.rs
@@ -1,3 +1,11 @@
+//! Expression part of the [FEF standard](https://github.com/jiricekcz/fef-specification/blob/main/expressions/Expression.md).
+//!
+//! FEF stores the formula as an expression tree in prefix notation.
+//! This library provides great modularity when it comes to storing expressions in memory using the [`Expr`] enum.
+//! Working with the [`Expr`] enum directly can be however unnecessarily verbose, if you don't need full control over the storage of child expressions.
+//! That's why this library provides the [`ExprTree`] wrapper, which represents children as `Box<ExprTree>` - the most intuitive way to store a tree structure in memory.
+//! Unless you have a special use case, [`ExprTree`] is probably the type you want to use.
+
 mod expr;
 mod exprs;
 mod read_from;
