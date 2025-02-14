3b030d8406454e5f9142cc92e7ed00d4c2099c8c
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2025-02-14 18:49:56 +0100

    docs for parse file

--- src/v0/parse/file.rs
+++ src/v0/parse/file.rs
@@ -6,6 +6,15 @@ use crate::v0::{
     traits::ReadFrom,
 };
 
+/// Reads a file from a reader to memory.
+///
+/// This is a convenience method that reads a file from a reader entirely into memory. It is supposed
+/// to suit most use cases, but if you need more control over the reading process, you will probably have
+/// to compose the reading process yourself from other parsing functions.
+///
+/// Note, that this function expects the version to have already been read from the reader.
+///
+/// This method outputs a [`File`] enum, which contains the parsed file.
 pub fn read_file<R: ?Sized + Read, C: ?Sized + Config>(
     reader: &mut R,
     configuration: &C,
@@ -13,6 +22,12 @@ pub fn read_file<R: ?Sized + Read, C: ?Sized + Config>(
     File::read_from(reader, configuration)
 }
 
+/// Reads a file from a reader to memory using the default configuration.
+///
+/// This is useful when dealing with arbitrary files, that expect default configuration.
+///
+/// Note, that this function expects the version to have already been read from the reader.
+/// For more information, see the [`read_file`] function.
 pub fn read_file_with_default_config<R: ?Sized + Read>(
     reader: &mut R,
 ) -> Result<File, FileReadError> {
