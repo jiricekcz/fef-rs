ddb397016f2f727b14083184424821bf24c446e4
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2025-02-15 17:29:19 +0200

    start of write docs

--- src/v0/write/configuration.rs
+++ src/v0/write/configuration.rs
@@ -16,14 +16,37 @@ macro_rules! write_enum_configuration {
     }};
 }
 
-/// Write a configuration to a byte stream.
+/// Write a [configuration](https://github.com/jiricekcz/fef-specification/blob/main/configuration/Configuration.md) to a byte stream.
 ///
-/// Writes any configuration to byte stream by writing all values.
+/// Writes any [`Config`] to byte stream by writing all values.
+///
+/// # Example
+///
+/// ```rust
+/// # use fef::v0::config::{Config, IntFormat, FloatFormat, OverridableConfig};
+/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// let mut configuration = OverridableConfig::default();
+///
+/// configuration.set_integer_format(IntFormat::Int8);
+/// configuration.set_float_format(FloatFormat::Float32);
+///
+/// let mut writer = Vec::new();
+///
+/// fef::v0::write::configuration::write_configuration(&mut writer, &configuration)?;
+///
+/// let reader = &mut writer.as_slice();
+/// let read_configuration = fef::v0::read::configuration::parse_configuration_with_default_configuration(reader)?;
+/// # Ok(())
+/// # }
 pub fn write_configuration<W: ?Sized + Write, C: ?Sized + Config>(
     writer: &mut W,
     configuration: &C,
 ) -> Result<(), ConfigurationWriteError> {
-    write_enum_configuration!(IntFormat, integer_format, writer, configuration);
+    let record_count = VariableLengthEnum::from(2);
+
+    record_count.write_to(writer, configuration)?;
+
     write_enum_configuration!(FloatFormat, float_format, writer, configuration);
+    write_enum_configuration!(IntFormat, integer_format, writer, configuration);
     Ok(())
 }
